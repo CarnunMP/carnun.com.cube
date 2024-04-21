b0ca8770ec0e297874bc30a0d96c1514b6b12b6d
CarnunMP <admin@carnun.com>
2024-04-21 12:50:59 +0100

    Polish: styling (including resizing), email, copy...

---
+++ foobarbaz.js
@@ -0,0 +1,4 @@
+const foo = "admin";
+const bar = "carnun";
+const baz = document.getElementById("baz");
+baz.href = `mailto:${foo}@${bar}.com`;
--- ice.js
+++ ice.js
@@ -122,12 +122,15 @@ function main() {
 
   // Asynchronously load an image
   var image = new Image();
-  image.src = "/cube2.png";
+  image.src = "/carnundotcube.png";
   image.addEventListener('load', function() {
     // Now that the image has loaded make copy it to the texture.
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.generateMipmap(gl.TEXTURE_2D);
+    //gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   });
 
   function degToRad(d) {
